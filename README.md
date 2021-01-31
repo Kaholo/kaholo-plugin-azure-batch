6f3dc61b0747b88322a7edc32bd7d8d57353bca8
Matan Kadosh <kadosh10001@gmail.com>
2021-01-31 18:00:44 +0200

    add clonePool and updatePool methods add autocomplete methods

--- README.md
+++ README.md
@@ -8,20 +8,75 @@ Azure Batch plugin for Kaholo
 3. Domain or Tenant ID
 4. Subscription ID
 
-## Method: createPool
+## Method: Create Pool
 
 **Description**
 
-This method will create a pool of compute nodes [For more info](https://docs.microsoft.com/enus/rest/api/batchmanagement/pool/create).
+This method will create a pool of compute nodes [For more info](https://docs.microsoft.com/en-us/rest/api/batchmanagement/pool/create).
 
 Pool nodes are the VMs that execute your tasks. Specify properties such as the number and size of the nodes, a Windows or Linux VM image, and an application to install when the nodes join the pool.
 
 **Parameters**
 
-1. Resource Group Name: The name of the resource group that contains the Batch account.
-2. Account Name: The name of the Batch account.
-3. Pool Name: The pool name. This must be unique within the account.
-4. Pool ID: (display name), The display name for the pool.
+1. Resource Group: The resource group that contains the Batch account.
+2. Batch Account: The Batch account to create the pool in.
+3. Pool Name: The new of the new pool name. This must be unique within the account.
+4. VM Size: (String), The size of virtual machines in the pool.
+5. Dedicated Nodes: (Integer), The desired number of dedicated compute nodes in the pool.
+At least one of targetDedicatedNodes, targetLowPriority nodes must be set.
+6. Low Priority Nodes: (Integer), The desired number of low-priority compute nodes in the pool.
+At least one of targetDedicatedNodes, targetLowPriority nodes must be set.
+
+A reference to an Azure Virtual Machines Marketplace image or the Azure Image resource of a custom Virtual Machine. To get the list of all imageReferences verified by Azure Batch, see the 'List supported node agent SKUs' operation.
+
+7. Image: The image to use for the new pool.
+
+8. Start task commandLine: (String) A task specified to run on each compute node as it joins the pool.
+9. Start task username: The user identity under which the start task runs.
+10. Start task env settings: (Array) A list of environment variable settings for the start task.
+11. Application: The application to be installed on each compute node in the pool.
+13. Application Version: The version of the application to be installed on each compute node in the pool.
+
+
+## Method: Delete Pool
+
+**Description**
+
+This method deletes a pool of compute nodes [For more info](https://docs.microsoft.com/en-us/rest/api/batchmanagement/pool/delete).
+
+**Parameters**
+
+1. Resource Group: The resource group that contains the Batch account.
+2. Batch Account: The Batch account to create the pool in.
+3. Pool Name: The name of the pool to delete.
+
+
+## Method: Deploy Package To Pool
+
+**Description**
+
+This method updates the pool with the new application package refrence. [For more info](https://docs.microsoft.com/en-us/rest/api/batchmanagement/pool/update).
+
+**Parameters**
+
+1. Resource Group: The resource group that contains the Batch account.
+2. Batch Account: The Batch account to create the pool in.
+3. Pool Name: The name of the pool to update.
+4. Application: The application to be installed on each compute node in the pool.
+5. Application Version: The version of the application to be installed on each compute node in the pool.
+
+## Method: Clone Pool
+
+**Description**
+
+This method gets the specification of an existing pool, and creates a new pool based on it. All parameters are optional and will be overriding the original pool options.
+
+**Parameters**
+
+1. Resource Group: The resource group that contains the Batch account.
+2. Batch Account: The Batch account to create the pool in.
+3. Pool: The pool to clone.
+4. New Pool Name: The new of the new pool name. This must be unique within the account.
 5. VM Size: (String), The size of virtual machines in the pool.
 6. Dedicated Nodes: (Integer), The desired number of dedicated compute nodes in the pool.
 At least one of targetDedicatedNodes, targetLowPriority nodes must be set.
@@ -30,17 +85,10 @@ At least one of targetDedicatedNodes, targetLowPriority nodes must be set.
 
 A reference to an Azure Virtual Machines Marketplace image or the Azure Image resource of a custom Virtual Machine. To get the list of all imageReferences verified by Azure Batch, see the 'List supported node agent SKUs' operation.
 
-8. Image publisher: (String) The publisher of the Azure Virtual Machines Marketplace image. For example, Canonical or MicrosoftWindowsServer.
-9. Image offer: (String) The offer type of the Azure Virtual Machines Marketplace image.For example, UbuntuServer or WindowsServer.
-10. Image sku: (String) The SKU of the Azure Virtual Machines Marketplace image. For example, 18.04-LTS or 2019-Datacenter.
-11. Image version: (String) The version of the Azure Virtual Machines Marketplace image.
-A value of 'latest' can be specified to select the latest version of an image. If omitted, the default is 'latest'.
-12. Node sku ID: (String) The SKU of the Batch node agent to be provisioned on compute nodes in the pool.
+8. Image: The image to use for the new pool.
 
-A task which is run when a compute node joins a pool in the Azure Batch service, or when the compute node is rebooted or reimaged.
-
-13. Start task commandLine: (String) A task specified to run on each compute node as it joins the pool.
-14. Start task username: The user identity under which the start task runs.
-15. Start task env settings: (Array) A list of environment variable settings for the start task.
-16. Application Packages: (Array) The list of application packages to be installed on each compute node in the pool.
-
+9. Start task commandLine: (String) A task specified to run on each compute node as it joins the pool.
+10. Start task username: The user identity under which the start task runs.
+11. Start task env settings: (Array) A list of environment variable settings for the start task.
+12. Application: The application to be installed on each compute node in the pool.
+13. Application Version: The version of the application to be installed on each compute node in the pool.
--- src/app.js
+++ src/app.js
@@ -1,85 +1,199 @@
-const msRest = require('@azure/ms-rest-nodeauth');
-const armBatch = require('@azure/arm-batch');
+const msRest = require("@azure/ms-rest-nodeauth");
+const armBatch = require("@azure/arm-batch");
+const { getBatchAccountNames, getResourceGroups, getApplications, getApplicationVersions, getImages, getPools} = require('./autocomplete');
+const parsers = require("./parsers");
 
 /**
  * Internal function for handling authentication and generation of batch managmnet client
- * @param {*} action 
- * @param {*} settings 
+ * @param {*} settings
  * @returns BatchManagementClient
  */
-function _getBatchClient(action, settings) {
-	/**
-	 * Create credentials from the clientId, secret and domain
-	 */
-    return msRest.loginWithServicePrincipalSecret(
-        settings.clientId, settings.secret, settings.domain).then(credentials => {
-			/**
-			 * Create new compute mamagement client using the credentials and subscription ID
-			 * And returns the new compute mamagement client
-			 */
-            const client = new armBatch.BatchManagementClient(credentials, settings.subscriptionId);
-            return client;
-        });
+async function _getBatchClient(settings) {
+    /**
+     * Create credentials from the clientId, secret and domain
+     */
+    const credentials = await msRest.loginWithServicePrincipalSecret(settings.clientId, settings.secret, settings.domain);
+
+    /**
+     * Create new compute mamagement client using the credentials and subscription ID
+     * And returns the new compute mamagement client
+     */
+    const client = new armBatch.BatchManagementClient(credentials, settings.subscriptionId);
+    return client;
 }
 
-function createPool(action, settings) {
-    return _getBatchClient(action, settings).then(batchClinet => {
-        let scaleSettings = {
-            fixedScale : {
-                targetDedicatedNodes : parseInt(action.params.targetDedicatedNodes),
-                targetLowPriorityNodes : parseInt(action.params.targetLowPriorityNodes)
-            }
-        }
+async function createPool(action, settings) {
+    const batchClinet = await _getBatchClient(settings);
+    const resourceGroupName = parsers.autocomplete(action.params.resourceGroupName);
+    const accountName = parsers.autocomplete(action.params.batchAccountName);
+    const poolName = action.params.poolName;
+    const application = parsers.autocomplete(action.params.application);
+    const applicationVersion = parsers.autocomplete(action.params.applicationVersion);
 
-        let deploymentConfiguration = {
-            virtualMachineConfiguration : {
-                imageReference : {
-                    publisher:action.params.imageReferencePublisher,
-                    offer:action.params.imageReferenceOffer,
-                    sku:action.params.imageReferenceSku,
-                    version:action.params.imageReferenceVersion
-                },
-                nodeAgentSkuId : action.params.nodeAgentSKUId
-            }
-        }
+    const scaleSettings = {
+        fixedScale: {
+            targetDedicatedNodes: parseInt(action.params.targetDedicatedNodes),
+            targetLowPriorityNodes: parseInt(action.params.targetLowPriorityNodes),
+        },
+    };
 
-        
-        var pool = { 
-            id: action.params.poolId, 
-            displayName: action.params.poolId, 
-            vmSize: action.params.vmSize,
-            scaleSettings : scaleSettings,
-            deploymentConfiguration : deploymentConfiguration,
-            applicationPackages : action.params.applicationPackages || []
+    let image;
+
+    if (!action.params.image){
+        throw "Image must be specified";
+    }
+
+    // If autocomplete
+    if (action.params.image.id){
+        try{
+            image = JSON.parse(action.params.image.id);
+        } catch (err){
+            throw "Invalid image ID supploed"
+        }
+    } else {
+        image = action.params.image;
+    }
+
+    const deploymentConfiguration = { 
+        virtualMachineConfiguration: {
+            imageReference: image.imageReference,
+            nodeAgentSkuId: image.nodeAgentSKUId,
+        }
+    };
+
+    var pool = {
+        id: action.params.poolName,
+        displayName: action.params.poolName,
+        vmSize: action.params.vmSize,
+        scaleSettings: scaleSettings,
+        deploymentConfiguration: deploymentConfiguration,
+        applicationPackages: [{
+            id : `/subscriptions/${settings.subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Batch/batchAccounts/${accountName}/applications/${application}`,
+            version: applicationVersion
+        }]
+    };
+
+    if (action.params.startTaskCommandLine) {
+        let startTask = {
+            commandLine: action.params.startTaskCommandLine,
+            userIdentity: {
+                userName: action.params.startTaskUsername,
+            },
+            environmentSettings: action.params.startTaskEnvironmentSettings || [],
             // [
             //     {
-            //         id : "id",
-            //         version : ""
+            //         name : "",
+            //         value : ""
             //     }
             // ]
         };
+        pool.startTask = startTask;
+    }
 
-        if(action.params.startTaskCommandLine){
-            let startTask = {
-                commandLine : action.params.startTaskCommandLine,
-                userIdentity : {
-                    userName : action.params.startTaskUsername
-                },
-                environmentSettings : action.params.startTaskEnvironmentSettings || []
-                // [
-                //     {
-                //         name : "",
-                //         value : ""
-                //     }
-                // ]
+    return batchClinet.pool.create(resourceGroupName, accountName, poolName, pool);
+}
+
+async function deletePool(action, settings) {
+    const resourceGroupName = action.params.resourceGroupName;
+    const accountName = action.params.accountName;
+    const poolName = action.params.poolName;
+    
+    const batchClinet = await _getBatchClient(settings);
+    return batchClinet.pool.deleteMethod(resourceGroupName, accountName, poolName);
+}
+
+async function deployPackageToPool(action, settings) {
+    const batchClinet = await _getBatchClient(settings);
+    const resourceGroupName = parsers.autocomplete(action.params.resourceGroupName);
+    const accountName = parsers.autocomplete(action.params.batchAccountName);
+    const application = parsers.autocomplete(action.params.application);
+    const applicationVersion = parsers.autocomplete(action.params.applicationVersion);
+    const poolName = action.params.poolName;
+
+    var pool = {
+        applicationPackages: [{
+            id : `/subscriptions/${settings.subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Batch/batchAccounts/${accountName}/applications/${application}`,
+            version: applicationVersion
+        }],
+    };
+
+    return batchClinet.pool.update(resourceGroupName, accountName, poolName, pool);
+}
+
+async function clonePool(action, settings) {
+    const batchClinet = await _getBatchClient(settings);
+    const resourceGroupName = parsers.autocomplete(action.params.resourceGroupName);
+    const accountName = parsers.autocomplete(action.params.batchAccountName);
+    const basePoolName = parsers.autocomplete(action.params.basePoolName);
+    const application = parsers.autocomplete(action.params.application);
+    const applicationVersion = parsers.autocomplete(action.params.applicationVersion);
+    const poolName = action.params.newPoolName;
+    
+    const pool = await batchClinet.pool.get(resourceGroupName,accountName,basePoolName);
+    const poolVirtualMachineConfiguration = pool.deploymentConfiguration.virtualMachineConfiguration;
+    
+    let startTask;
+    if (action.params.startTaskCommandLine) {
+        const isStartTaskExist = !!pool.startTask
+        const environmentSettings = action.params.startTaskEnvironmentSettings || (isStartTaskExist? pool.startTask.environmentSettings : []);
+        
+        startTask = {
+            commandLine: action.params.startTaskCommandLine,
+            userIdentity: isStartTaskExist? pool.startTask.userIdentity : undefined,
+            environmentSettings: environmentSettings
+        };
+    };
+
+    let image;
+    // If autocomplete
+    if(action.params.image){
+        if (action.params.image.id){
+            try{
+                image = JSON.parse(action.params.image.id);
+            } catch (err){
+                throw "Invalid image ID supploed"
             }
-            pool.startTask = startTask;
+        } else {
+            image = action.params.image;
         }
+    }
+        
+    var pool = {
+        id: action.params.poolName,
+        displayName: action.params.poolName,
+        vmSize: action.params.vmSize || pool.vmSize,
+        scaleSettings: {
+            fixedScale: {
+                targetDedicatedNodes: parseInt(action.params.targetDedicatedNodes) || pool.scaleSettings.fixedScale.targetDedicatedNodes,
+                targetLowPriorityNodes: parseInt(action.params.targetLowPriorityNodes) || pool.scaleSettings.fixedScale.targetLowPriorityNodes,
+            },
+        },
+        deploymentConfiguration: { 
+            virtualMachineConfiguration: image ? {
+                imageReference: image.imageReference,
+                nodeAgentSkuId: image.nodeAgentSKUId,
+            } : poolVirtualMachineConfiguration
+        },
+        applicationPackages: [{
+            id : `/subscriptions/${settings.subscriptionId}/resourceGroups/${resourceGroupName}/providers/Microsoft.Batch/batchAccounts/${accountName}/applications/${application}` || pool.applicationPackages[0].id,
+            version: applicationVersion || pool.applicationPackages[0].version
+        }],
+        startTask: startTask || pool.startTask
+    };
 
-        return batchClinet.pool.create(action.params.resourceGroupName, action.params.accountName, action.params.poolName, pool)
-    })
+    return batchClinet.pool.create(resourceGroupName, accountName, poolName, pool);
 }
 
 module.exports = {
-    createPool: createPool
-}
+    createPool,
+    deletePool,
+    deployPackageToPool,
+    clonePool,
+    // Autocomplete
+    getBatchAccountNames,
+    getResourceGroups,
+    getApplications,
+    getApplicationVersions,
+    getImages,
+    getPools
+};
